b12b69cf9f30fe28edbbf31fc2687af86a9ffae8
fxrhannn <dangerfsgaming@gmail.com>
2025-02-09 22:55:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const button = document.getElementById('clickMeBtn');
 
 // Add a click event listener to the button
 button.addEventListener('click', function() {
-    alert("Happy Teddy Day! 🎉🐼💖");
+    alert("Happy Teddy Day! 🎉🐼💖 Hope you have a wonderful day full of love and joy!");
 });

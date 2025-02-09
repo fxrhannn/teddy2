43d86bc943fa785b41b2d88efab6eb0a309bb1a0
fxrhannn <dangerfsgaming@gmail.com>
2025-02-09 22:44:28 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Get the button element
+const button = document.getElementById('clickMeBtn');
+
+// Add a click event listener to the button
+button.addEventListener('click', function() {
+    alert("Happy Teddy Day! 🎉🐼💖");
+});
